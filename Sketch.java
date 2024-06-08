746d77ddb959185051207b7f5440ea5521b42c60
caleb <duocaleb3@gmail.com>
2024-06-07 22:52:46 -0400

    Resubmit

--- Sketch.java
+++ Sketch.java
@@ -1,4 +1,6 @@
 import processing.core.PApplet;
+import processing.event.MouseEvent;
+
 import java.util.ArrayList;
 
 public class Sketch extends PApplet {
@@ -7,13 +9,16 @@ public class Sketch extends PApplet {
   int[] intCharPos = new int[2];
   ArrayList<int[]> intEllipses = new ArrayList<>();
   int intPlayerSpeed = 1;
+  int intRed = 0;
+  int intAdd = 1;
+  int intScreenSize = 400;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(intScreenSize, intScreenSize);
   }
 
   /** 
@@ -22,6 +27,7 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+    noStroke();
   }
 
   /**
@@ -30,6 +36,7 @@ public class Sketch extends PApplet {
   public void draw() {
     clear();
     background(210, 255, 173);
+
     if(boolKeyHeld[(int)'a']){
       intCharPos[0] -= intPlayerSpeed;
     }
@@ -42,23 +49,34 @@ public class Sketch extends PApplet {
     if(boolKeyHeld[(int)'s']){
       intCharPos[1] += intPlayerSpeed;
     }
+
     if(boolKeyHeld[255]){
       intPlayerSpeed = 2;
     }
     else{
       intPlayerSpeed = 1;
     }
+
     if(mousePressed){
       intEllipses.add(new int[]{intCharPos[0],intCharPos[1]});
     }
+
     for(int x = 0; x < intEllipses.size(); x++){
       ellipse(intEllipses.get(x)[0],intEllipses.get(x)[1],10,10);
     }
-    if(boolKeyHeld[(int)'c']){
-      intEllipses.clear();
+
+    fill(color(intRed,intScreenSize/255*mouseX,intScreenSize/255*mouseY));
+    
+    if(keyPressed){
+      intRed += intAdd*intPlayerSpeed;
+      if(intRed > 255 || intRed <= 0){
+        intAdd *= -1;
+      }
     }
+
     ellipse(intCharPos[0], intCharPos[1], 10, 10);
   }
+ 
   
   @Override
   public void keyPressed() {
@@ -68,9 +86,10 @@ public class Sketch extends PApplet {
       }
     }
     if(key != CODED){
-      boolKeyHeld[(int)key] = true;
+      boolKeyHeld[(int)(Character.toLowerCase(key))] = true;
     }
   }
+
   @Override
   public void keyReleased() {
     if (key == CODED){
@@ -79,7 +98,12 @@ public class Sketch extends PApplet {
       }
     }
     else{
-      boolKeyHeld[(int)key] = false;
+      boolKeyHeld[(int)(Character.toLowerCase(key))] = false;
     }
+  }
+  
+  @Override
+  public void mouseReleased() {
+    intEllipses.clear();
   }
 }
